af8149a810520d7e90559a86ed7ab91687d284ec
Paulo Novo <paulomnovo02@gmail.com>
2026-01-08 16:44:42 +0000

    Add AR audio and refactor AR scene logic
    Added new audio file for AR experience. Refactored AR scene injection and 3D model logic in scripts.js for better organization and reliability. Updated CSS for AR scene and video layering. Improved event handling for AR interactions and map resizing.

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,8 +1,7 @@
-var map;
+var map = null;
 var dados = [];
-var userMarker;
+var userMarker = null;
 var userPos = null;
-var somAtual = null;
 var pontoMaisProximo = null;
 var primeiraVezGPS = true;
 var arCarregado = false;
@@ -14,11 +13,6 @@ window.onload = async () => {
 
         iniciarMapa();
         monitorizarGPS();
-
-        const scene = document.querySelector('a-scene');
-        if (scene) {
-            scene.pause();
-        }
 
     } catch (e) {
         console.error("Erro:", e);
@@ -46,9 +40,7 @@ function iniciarMapa() {
         bounceAtZoomLimits: false
     });
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap contributors' }).addTo(map);
 
     dados.forEach(ponto => {
         var iconUrl = (ponto.assets.imagemIcone == null || ponto.assets.imagemIcone == "") ? 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png' : "./assets/imgs/" + ponto.assets.imagemIcone;
@@ -58,29 +50,9 @@ function iniciarMapa() {
 
         var marker = L.marker([ponto.coords.lat, ponto.coords.lng], { icon: icon }).addTo(map);
         marker.bindPopup(`<div class="text-center"><b>${ponto.titulo}</b><br>${ponto.descricao}</div>`);
+
+        setTimeout(() => map.invalidateSize(), 200);
     });
-}
-
-// efeito mira css
-function configurarEventosMira() {
-    const cursor3D = document.querySelector('#cursor-3d');
-    const miraCSS = document.querySelector('#mira-central');
-
-    if (cursor3D && miraCSS) {
-        cursor3D.addEventListener('fusing', function () {
-            miraCSS.classList.add('focando');
-        });
-
-        cursor3D.addEventListener('mouseleave', function () {
-            miraCSS.classList.remove('focando');
-        });
-
-        cursor3D.addEventListener('click', function () {
-            miraCSS.classList.remove('focando');
-            miraCSS.style.backgroundColor = 'rgba(0, 255, 0, 0.8)';
-            setTimeout(() => { miraCSS.style.backgroundColor = 'white'; }, 300);
-        });
-    }
 }
 
 function monitorizarGPS() {
@@ -89,8 +61,8 @@ function monitorizarGPS() {
         userPos = { lat: pos.coords.latitude, lng: pos.coords.longitude };
 
         if (!userMarker) {
-            var i = L.divIcon({ className: 'user-marker', iconSize: [15, 15] });
-            userMarker = L.marker([userPos.lat, userPos.lng], { icon: i }).addTo(map);
+            const icon = L.divIcon({ className: 'user-marker', iconSize: [15, 15] });
+            userMarker = L.marker([userPos.lat, userPos.lng], { icon }).addTo(map);
         } else {
             userMarker.setLatLng([userPos.lat, userPos.lng]);
         }
@@ -118,178 +90,176 @@ function atualizarBotao() {
         }
     });
 
-    if (menorDistancia < 20) {
-        pontoMaisProximo = tempPonto;
+    pontoMaisProximo = (menorDistancia < 20) ? tempPonto : null;
+
+    if (pontoMaisProximo) {
         if (!btn.classList.contains('ready')) {
             btn.classList.add('ready', 'btn-agitar');
             if (navigator.vibrate) navigator.vibrate(200);
         }
     } else {
         btn.classList.remove('ready', 'btn-agitar');
-        pontoMaisProximo = null;
     }
 }
 
-function iniciarAR(ponto) {
-
-    if (!ponto) return;
-
-    const scene = document.querySelector('a-scene');
-
-    // limpar anteriores
-    const markersAntigos = scene.querySelectorAll('a-marker');
-
-    for (const marker of markersAntigos) {
-        marker.remove();
+// dividi a animacao da dos eventos para ficar mais organizado
+function animarMira(estado) {
+    const mira = document.getElementById('mira-central');
+    if (estado === 'focar') mira.classList.add('focando');
+    if (estado === 'normal') mira.classList.remove('focando');
+    if (estado === 'sucesso') {
+        mira.classList.remove('focando');
+        mira.style.backgroundColor = 'rgba(0, 255, 0, 0.8)';
+        setTimeout(() => { mira.style.backgroundColor = 'white'; }, 300);
     }
-
-    // cria o marker do ponto
-    const marker = document.createElement('a-marker');
-    marker.setAttribute('type', 'barcode');
-    marker.setAttribute('value', ponto.assets.markerValue);
-
-    marker.setAttribute('smooth', 'true');
-    marker.setAttribute('smoothCount', '10');
-    marker.setAttribute('smoothTolerance', '0.01');
-    marker.setAttribute('smoothThreshold', '5');
-
-
-    // ira ser um soldado a falar afinal
-    const animacoesFala = ["Talking_1", "Talking_2", "Talking_3", "Talking_4"];
-
-    const caminhoDoModelo = 'assets/models/Soldado.glb';
-
-    const modelo = document.createElement('a-entity');
-    modelo.setAttribute('gltf-model', caminhoDoModelo);
-    modelo.setAttribute('scale', '3 3 3');
-    modelo.setAttribute('position', '0 0 0');
-    modelo.setAttribute('class', 'clicavel');
-
-    // ele está em idle primeiro so se lhe m,riarem fala
-    modelo.setAttribute('animation-mixer', 'clip: Idle; loop: repeat; crossFadeDuration: 0.5');
-    modelo.setAttribute('look-at', '[camera]');
-
-
-    // precisa da hitbox para o click ser melhorzinho senao so o solado e lixado
-    const hitbox = document.createElement('a-box');
-    hitbox.setAttribute('position', '0 0.5 0');
-    hitbox.setAttribute('scale', '3 3.5 3');
-    hitbox.setAttribute('material', 'opacity: 0; transparent: true');
-    hitbox.setAttribute('class', 'clicavel');
-
-    marker.appendChild(modelo);
-    marker.appendChild(hitbox);
-
-    console.log("o soldado foi posto");
-
-    if (ponto.assets.audio) {
-        marker.setAttribute('sound', `src: url(${ponto.assets.audio}); autoplay: false; volume: 2`);
-
-        hitbox.addEventListener('click', function () {
-            let soundComp = marker.components.sound;
-
-            if (soundComp && !soundComp.playing) {
-                soundComp.playSound();
-
-                // escolhe uma animcacao 
-                const animacaoSorteada = animacoesFala[Math.floor(Math.random() * animacoesFala.length)];
-
-                modelo.setAttribute('animation-mixer', `clip: ${animacaoSorteada}; loop: repeat; crossFadeDuration: 0.5`);
-            }
-        });
-
-        marker.addEventListener('sound-ended', function () {
-            modelo.setAttribute('animation-mixer', 'clip: Idle; loop: repeat; crossFadeDuration: 0.5');
-        });
-
-    }
-
-    scene.appendChild(marker);
 }
 
-function abrirAR() {
-    // esconde mapa e o botao
+function configurarEventosMira() {
+    const cursor = document.querySelector('#cursor-3d');
+    if (!cursor) return;
+    cursor.addEventListener('fusing', () => animarMira('focar'));
+    cursor.addEventListener('mouseleave', () => animarMira('normal'));
+    cursor.addEventListener('click', () => animarMira('sucesso'));
+}
+
+// --- CONTROLO AR ---
+async function abrirAR() {
+    if (!pontoMaisProximo) return;
+
+    // UI
     document.getElementById('modo-mapa').classList.add('d-none');
     document.getElementById('modo-mapa').classList.remove('d-flex');
     document.getElementById('btn-ar-floating').classList.add('d-none');
     document.getElementById('btn-ar-floating').classList.remove('d-flex');
-
     document.getElementById('modo-ar').classList.remove('d-none');
 
     if (!arCarregado) {
-        injetarCenaAR();
-    } else {
-        const scene = document.querySelector('a-scene');
-        if (scene) scene.play();
-
-        console.log("Reiniciando AR para:", pontoMaisProximo);
-        iniciarAR(pontoMaisProximo);
+        await injetarCenaAR();
     }
-}
 
-// isto so porque se a cena fosse logo carregada dava erro ao iniciar na pagina de mostrar meio mapa e meia camara na pagina que fica tudo mal
-// por isso so carrega a cena ao iniciar ar que evita esse problema, tendo o placeholder a dizer que esta a carrregar nao fica mal
-function injetarCenaAR() {
-    // mostra o loading
-    document.getElementById('ar-loading').classList.remove('d-none');
+    const scene = document.querySelector('a-scene');
+    if (scene) scene.play();
 
-    const placeholder = document.getElementById('ar-scene-placeholder');
-
-    const htmlCena = `
-        <a-scene embedded
-            arjs="sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3;"
-            vr-mode-ui="enabled: false"
-            renderer="logarithmicDepthBuffer: true;">
-            
-            <a-assets id="assets-loader"></a-assets>
-            
-            <a-entity camera>
-                <a-cursor id="cursor-3d" fuse="true" fuseTimeout="1500" material="opacity: 0; transparent: true"
-                    raycaster="objects: .clicavel"></a-cursor>
-            </a-entity>
-        </a-scene>
-    `;
-
-    // meto a cena agora na pagina
-    placeholder.innerHTML = htmlCena;
-    arCarregado = true;
-
-    setTimeout(() => {
-        console.log("a iniciar o AR depois do loading", pontoMaisProximo);
-        iniciarAR(pontoMaisProximo);
-
-        document.getElementById('ar-loading').classList.add('d-none');
-        configurarEventosMira();
-    }, 2000);
+    iniciarCena3D(pontoMaisProximo);
 }
 
 function fecharAR() {
-    // esconde ar
     document.getElementById('modo-ar').classList.add('d-none');
-
-    // mostra mapa e o botao
     document.getElementById('modo-mapa').classList.remove('d-none');
     document.getElementById('modo-mapa').classList.add('d-flex');
-
     document.getElementById('btn-ar-floating').classList.remove('d-none');
     document.getElementById('btn-ar-floating').classList.add('d-flex');
 
     const scene = document.querySelector('a-scene');
     if (scene) {
-        if (scene.components.sound) scene.components.sound.stopSound();
-
-        const markersAntigos = scene.querySelectorAll('a-marker');
-        for (const marker of markersAntigos) {
-            if (marker.components.sound) marker.components.sound.stopSound();
-            marker.remove();
-        }
+        // Para todos os sons
+        const sons = scene.querySelectorAll('[sound]');
+        sons.forEach(s => s.components.sound.stopSound());
         scene.pause();
     }
 
-    scene.pause();
-    // da reload ao mapa, porque ele fica bugado ao voltar
-    setTimeout(() => {
-        if (map) map.invalidateSize();
-        if (userPos) map.setView([userPos.lat, userPos.lng], 16);
-    }, 100);
+    setTimeout(() => map.invalidateSize(), 100);
+}
+
+function injetarCenaAR() {
+    document.getElementById('ar-loading').classList.remove('d-none');
+    const placeholder = document.getElementById('ar-scene-placeholder');
+
+    const isPortrait = window.innerHeight > window.innerWidth;
+    const w = isPortrait ? 960 : 1280;
+    const h = isPortrait ? 1280 : 960;
+
+    // Removi o 'look-controls' da câmara porque sem sticky não precisamos de giroscópio
+    const html = `
+        <a-scene embedded
+            arjs="sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3; 
+            sourceWidth:${w}; sourceHeight:${h}; displayWidth:${w}; displayHeight:${h};"
+            vr-mode-ui="enabled: false"
+            renderer="logarithmicDepthBuffer: true;">
+            <a-assets id="assets-loader"></a-assets>
+            <a-entity camera>
+                <a-cursor id="cursor-3d" fuse="true" fuseTimeout="1500" material="opacity: 0; transparent: true" raycaster="objects: .clicavel"></a-cursor>
+            </a-entity>
+        </a-scene>`;
+
+    placeholder.innerHTML = html;
+    arCarregado = true;
+
+    return new Promise((resolve) => {
+        const scene = document.querySelector('a-scene');
+        const onLoaded = () => {
+
+            configurarEventosMira();
+            window.dispatchEvent(new Event('resize'));
+            document.getElementById('ar-loading').classList.add('d-none');
+            resolve();
+        };
+        const fallback = setTimeout(onLoaded, 3000);
+        scene.addEventListener('loaded', () => {
+            clearTimeout(fallback);
+            onLoaded();
+        });
+    });
+}
+
+// estava super confuso entao refatoriz<ei 
+function iniciarCena3D(ponto) {
+    const scene = document.querySelector('a-scene');
+
+    // limpa tudo que tinha dos markers
+    const antigos = scene.querySelectorAll('a-marker');
+    antigos.forEach(el => el.remove());
+
+    // cria o novo marker
+    const marker = document.createElement('a-marker');
+    marker.setAttribute('type', 'barcode');
+    marker.setAttribute('value', ponto.assets.markerValue);
+    marker.setAttribute('smooth', 'true');
+
+    // cria o soldado 3d
+    const modelo = document.createElement('a-entity');
+    modelo.setAttribute('gltf-model', 'assets/models/Soldado.glb');
+    modelo.setAttribute('scale', '1 1 1');
+    // modelo.setAttribute('look-at', '[camera]');
+    modelo.setAttribute('animation-mixer', 'clip: Idle; loop: repeat; crossFadeDuration: 0.5');
+    modelo.setAttribute('class', 'clicavel');
+
+    // precisa da hitbox para o click ser melhorzinho senao so o solado e lixado
+    const hitbox = document.createElement('a-box');
+    hitbox.setAttribute('position', '0 0.5 0');
+    hitbox.setAttribute('scale', '1 1.5 1');
+    hitbox.setAttribute('material', 'opacity: 0; transparent: true');
+    hitbox.setAttribute('class', 'clicavel');
+
+    // trata di audio
+    if (ponto.assets.audio) {
+        // o som ta no marker, se ele sai o som tb acaba
+        marker.setAttribute('sound', `src: url(./assets/audios/${ponto.assets.audio}); autoplay: false; volume: 2`);
+
+        hitbox.addEventListener('click', () => {
+            const som = marker.components.sound;
+            if (som && !som.playing) {
+                som.playSound();
+                // animcao aleatoria entre estas par ao solado
+                const anims = ["Talking_1", "Talking_2", "Talking_3", "Talking_4"];
+                const anim = anims[Math.floor(Math.random() * anims.length)];
+                modelo.setAttribute('animation-mixer', `clip: ${anim}; loop: repeat; crossFadeDuration: 0.5`);
+            }
+        });
+
+        marker.addEventListener('sound-ended', () => {
+            modelo.setAttribute('animation-mixer', 'clip: Idle; loop: repeat; crossFadeDuration: 0.5');
+        });
+
+        // marker desaparece o som para
+        marker.addEventListener('markerLost', () => {
+            const som = marker.components.sound;
+            if (som) som.stopSound();
+        });
+    }
+
+    marker.appendChild(modelo);
+    marker.appendChild(hitbox);
+
+    scene.appendChild(marker);
 }
